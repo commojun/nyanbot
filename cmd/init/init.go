8185ed261b3432ee4a70dcb51c7b199a6b0e3a05
commojun <junpooooow@gmail.com>
2019-08-11 03:03:53 +0900

    initでredisにつながるまでしばらくRetryするようにした

--- cmd/init/init.go
+++ cmd/init/init.go
@@ -2,14 +2,30 @@ package main
 
 import (
 	"log"
+	"time"
 
+	"github.com/Songmu/retry"
+	"github.com/commojun/nyanbot/app/redis"
 	"github.com/commojun/nyanbot/masterdata/key_value"
 	"github.com/commojun/nyanbot/masterdata/table"
 )
 
 func main() {
+	// Redisに接続できるか
+	rc := redis.NewClient()
+	err := retry.Retry(10, 10*time.Second, func() error {
+		log.Println("attempt to connect to redis")
+		err := rc.Keys("*").Err()
+		return err
+	})
+	if err != nil {
+		log.Println("failed to connect to redis")
+		log.Fatal(err)
+	}
+	log.Println("redis connection ok")
+
 	log.Println("Table initialize")
-	_, err := table.Initialize()
+	_, err = table.Initialize()
 	if err != nil {
 		log.Fatal(err)
 	}
