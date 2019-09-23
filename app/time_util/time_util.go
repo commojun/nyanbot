70b85bbfa2881142b9f66e0102dec95c7b475e0d
commojun <junpooooow@gmail.com>
2019-09-23 19:13:18 +0900

    alpineでtimezoneが使えなかったので、fixedzomeを使うようにした

--- app/time_util/time_util.go
+++ app/time_util/time_util.go
@@ -1,25 +1,28 @@
 package time_util
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 var nyantime = struct {
-	Layout      string
-	LocationStr string
+	Layout         string
+	LocationName   string
+	LocationOffset int
 }{
-	Layout:      "2006-01-02 15:04:05",
-	LocationStr: "Asia/Tokyo",
+	Layout:         "2006-01-02 15:04:05",
+	LocationName:   "JST",
+	LocationOffset: 9 * 60 * 60,
 }
 
 func JSTParse(s string) (time.Time, error) {
-	l, err := time.LoadLocation(nyantime.LocationStr)
-	if err != nil {
-		return time.Time{}, err
-	}
+	l := time.FixedZone(nyantime.LocationName, nyantime.LocationOffset)
 
 	t, err := time.ParseInLocation(nyantime.Layout, s, l)
 	if err != nil {
 		return time.Time{}, err
 	}
+	fmt.Println(t)
 	return t, nil
 }
 
